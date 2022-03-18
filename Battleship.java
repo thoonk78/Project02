b287827d52ae0f5b9583ff46209d9adccd95db42
thoonk78 <100811955+thoonk78@users.noreply.github.com>
2022-03-18 14:27:20 -0500

    Update Battleship.java

--- Battleship.java
+++ Battleship.java
@@ -1,8 +1,8 @@
 import java.util.Scanner;
 /**
- * Write a description of class Battleship here.
+ * Carries out operations of the Battleship game
  *
- * @author ()
+ * @author Keith Thoong, Samuel Ayoade
  * @version (a version number or a date)
  */
 public class Battleship extends DuoPlay
@@ -14,33 +14,32 @@ public class Battleship extends DuoPlay
     
     Scanner scnr = new Scanner (System.in);
     char[][] p1Board;
-    char[][] p1BoardSeen;
     char[][] p2Board;
-    char[][] p2BoardSeen;
     char[][] currBoard;
     int[][] boatNum;
     boolean isPlayer1Turn;
+    /**
+     * Battleship game constructor
+     * 
+     * @param player1 the first player
+     * @param player2 the second player
+     * @param size the size of the board
+     */
     public Battleship(BattleshipPlayer player1, BattleshipPlayer player2, int size){
         super((DuoplayPlayer)player1, (DuoplayPlayer)player2);
         isPlayer1Turn = true;
-        int[][] boatNum = {{5},{4},{3,3},{2}};
-        this.boatNum = boatNum;
-        /*
-        System.out.println("What board size would you like to play on? : ");
-        size = scnr.nextInt();
-        */
+        this.boatNum = new int[][]{{5},{4},{3,3},{2}};
         
         while(size < 0)
         {
             System.out.println("The board size must be positive");
             System.out.println("What board size would you like to play on? : ");
             size = scnr.nextInt();
+            scnr.nextLine();
         }
         
         this.p1Board = new char [size][size];
         this.p2Board = new char [size][size];
-        this.p1BoardSeen = new char [size][size];
-        this.p2BoardSeen = new char [size][size];
         this.currBoard = p1Board;
         
         for(int i = 0; i < p1Board.length; i++){
@@ -54,15 +53,18 @@ public class Battleship extends DuoPlay
         System.out.println(super.player1.getPlayerName() + ", set up your boats. " + super.player2.getPlayerName() + ", no peeking!");
         setBoats();
         showGame(true);
-        System.out.println("\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n");
+        System.out.println("\n\n\n\n\n\n\n\n\n\n\n\n\n\n\\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n");
         toggleTurn();
         System.out.println(super.player2.getPlayerName() + ", set up your boats. " + super.player1.getPlayerName() + ", no peeking!");
         setBoats();
         showGame(true);
-        System.out.println("\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n");
+        System.out.print("\n\n\n\n\n\n\n\n\n\n\n\n\n\n\\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n");
         toggleTurn();
         
     }
+    /**
+     * Sets the boats on the player's board
+     */
     public void setBoats()
     {
         showGame();
@@ -76,17 +78,16 @@ public class Battleship extends DuoPlay
             for(int j = 0; j < boatNum[i].length; j++)
             {
                 System.out.print("Do you want your boat to be horizontal? : ");
-                //System.out.println();
                 userChoice = scnr.nextLine();
-                //System.out.println();
                 
+                //Checks if user input is invalid and prompts user to enter inputs again if invalid
                 while(!(userChoice.toLowerCase().equals("yes")) && !(userChoice.toLowerCase().equals("no")))
                 {
                     System.out.println("Invalid. Please enter yes or no");
                     System.out.println(userChoice);
                     userChoice = scnr.nextLine();
                 }
-                
+                //Checks if user wants to place a boat horizontally or vertically
                 if((userChoice.toLowerCase()).equals("yes"))
                 {
                     isHorizontal = true;   
@@ -105,6 +106,7 @@ public class Battleship extends DuoPlay
                         System.out.println("Invalid row. Try again.");  
                         System.out.print("top end row: ");
                         firstRow = scnr.nextInt();
+                        scnr.nextLine();
                     }
                     System.out.print("top end column: ");
                     firstCol = scnr.nextInt();
@@ -113,6 +115,7 @@ public class Battleship extends DuoPlay
                         System.out.println("Invalid col. Try again.");  
                         System.out.print("top end col: ");
                         firstCol = scnr.nextInt();
+                        scnr.nextLine();
                     }
                     fillBoat(firstRow, firstCol, boatNum[i][j], isHorizontal);
                 }
@@ -121,8 +124,10 @@ public class Battleship extends DuoPlay
                     System.out.println("Pick the coordinates for the top end of your " + boatNum[i][j] + " space boat\n");
                     System.out.print("left end row: ");
                     firstRow = scnr.nextInt();
+                    scnr.nextLine();
                     System.out.print("left end column: ");
                     firstCol = scnr.nextInt();
+                    scnr.nextLine();
                     fillBoat(firstRow, firstCol, boatNum[i][j], isHorizontal);
                 }
             }
@@ -131,10 +136,16 @@ public class Battleship extends DuoPlay
        
         
     }
+    /**
+     * Fills the rest of the boat to its size
+     * 
+     * @param row the row of the first point of the boat
+     * @param col the column of the first point of the boat
+     */
     public void fillBoat(int row, int col, int limit, boolean isHorizontal)
     {
         boolean isEmpty = lineEmpty(row, col, limit, isHorizontal);
-        
+        //prompts user to enter positions again if boat overlaps
         if(isEmpty == false)
         {
             System.out.println("Sorry, this boat overlaps another boat or the edge. Try again.");
@@ -144,7 +155,7 @@ public class Battleship extends DuoPlay
             col = scnr.nextInt();
             fillBoat(row, col, limit, isHorizontal);
         }
-        if(isHorizontal)
+        if(isHorizontal)//fills boat by size horizontally
         {
             if(isEmpty)
             {
@@ -157,7 +168,7 @@ public class Battleship extends DuoPlay
                 }
             }
         }
-        else
+        else//fills boat by size vertically
         {
             if(isEmpty)
             {
@@ -173,6 +184,9 @@ public class Battleship extends DuoPlay
         showGame(true);
         System.out.println("\n");
     }
+    /**
+     * Checks if the boat overlaps with another boat or the board border
+     */
     public boolean lineEmpty(int row, int col, int limit, boolean isHorizontal)
     {
         boolean lineEmpty = true;
@@ -213,8 +227,13 @@ public class Battleship extends DuoPlay
         return lineEmpty;
         
     }
+    /**
+     * Checks if the player's guess is a hit or a miss
+     * 
+     * @param row the row of the position to strike
+     * @param col the column of the position to strike
+     */
     public void press(int row, int col){
-        //toggleTurn();
         while(row < 0 || row > currBoard.length - 1 || col < 0 || col > currBoard.length - 1)
         {
             System.out.println("Invalid input. Please enter a row and column from the coordinates shown");
@@ -234,8 +253,10 @@ public class Battleship extends DuoPlay
             currBoard[row][col] = MISS;   
             System.out.println("Miss!\n\n");
         }
-        //toggleTurn();
     }
+    /**
+     * Toggles the board that the player is trying to strike on
+     */
     public void toggleTurn()
     {
         isPlayer1Turn = !isPlayer1Turn;
@@ -249,6 +270,12 @@ public class Battleship extends DuoPlay
             currBoard = p2Board;   
         }
     }
+    /**
+     * Retruns the String formatted board
+     * 
+     * @param showBoats declares if the boat positions should be shown
+     * @return the String formatted board
+     */
     public String toString(boolean showBoats)
     {
         String playerName;
@@ -271,7 +298,7 @@ public class Battleship extends DuoPlay
         for(int i = 0; i < currBoard.length; i++){
             boardOut += String.format("%-2d ", i);
             for(int j = 0; j < currBoard.length; j++){
-                if(!showBoats && currBoard[i][j] == BOAT)
+                if(!showBoats && currBoard[i][j] == BOAT)//masks the boat position with water
                 {
                     boardOut += String.format("%-2c ", WATER);
                 }
@@ -283,12 +310,14 @@ public class Battleship extends DuoPlay
             boardOut += "\n";
         }
         
-        //System.out.println(this.board.toString());
         return "   " + playerName + "'s board\n" + boardOut;
     }
-    
-    
-        public boolean noBoats(){
+    /**
+     * Checks if all boats have been hit
+     * 
+     * @return a boolean for if there are still boats left
+     */
+    public boolean noBoats(){
         boolean noBoats = true;
         for(int i = 0; i < currBoard.length; i++){
             for(int j = 0; j < currBoard[i].length / 2; j++){
@@ -300,11 +329,18 @@ public class Battleship extends DuoPlay
         super.isPlayer1Turn = !super.isPlayer1Turn;
         return noBoats;
     }
-    
+    /**
+     * Prints the board
+     * 
+     * @param showBoats declares if the boat positions should be shown
+     */
     public void showGame(boolean showBoats)
     {
         System.out.println(toString(showBoats));
     }
+    /**
+     * Prints the board
+     */
     public void showGame()
     {
         System.out.println(toString(false));
